03b6237a2ecf038bf7bb6a0a2277fcf29060eb2d
Sam Pratt <Sam Pratt>
2016-12-07 18:45:24 +0000

    Initial commit

---
+++ app/src/main/java/com/sudoku/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sudoku;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void TakeAPicture(View v) throws Exception {
+        Intent takeAPictureIntent = new Intent(this, TakeAPictureActivity.class);
+        startActivity(takeAPictureIntent);
+    }
+}
---
+++ app/src/main/java/com/sudoku/TakeAPictureActivity.java
@@ -0,0 +1,35 @@
+package com.sudoku;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+public class TakeAPictureActivity extends Activity{
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takeapicture);
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = (ImageView)findViewById(R.id.PreviewImageView);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
