e0cf90f284576ea19364e12fed95a39ffb787f6b
samuelpratt <samuel.pratt@gmail.com>
2016-12-13 12:25:09 +0000

    Fixed OpenCV dependency.

---
+++ puzzlescanner/src/main/java/com/sudoku/puzzlescanner/ColourFilter.java
@@ -0,0 +1,4 @@
+package com.sudoku.puzzlescanner;
+
+public class ColourFilter {
+}
--- scanner/src/main/java/com/sudoku/scanner/ColourFilter.java
+++
@@ -1,8 +0,0 @@
-package com.sudoku.scanner;
-
-/**
- * Created by samuelpratt on 12/12/2016.
- */
-
-public class ColourFilter {
-}
