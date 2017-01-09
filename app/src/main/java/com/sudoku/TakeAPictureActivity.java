522731a33c2caba39e5521322f4e5b9aee0b0c34
samuelpratt <samuel.pratt@gmail.com>
2017-01-09 23:21:12 +0000

    Got picture capture working.

--- app/src/main/java/com/sudoku/TakeAPictureActivity.java
+++ app/src/main/java/com/sudoku/TakeAPictureActivity.java
@@ -3,13 +3,24 @@ package com.sudoku;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 //Based on https://developer.android.com/training/camera/photobasics.html
 public class TakeAPictureActivity extends Activity{
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,17 +31,59 @@ public class TakeAPictureActivity extends Activity{
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(null, "Error occurred while creating the File", ex);
+            }
+            // Continue only if the File was successfully created
+            Uri photoURI = null;
+            if (photoFile != null) {
+                try {
+                    photoURI = FileProvider.getUriForFile(getApplicationContext(), "com.sudoku.fileprovider", photoFile);
+                } catch (Exception ex) {
+                    Log.e(null, "An error occurred getting the URI for the image file", ex);
+                }
+                try {
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } catch (Exception ex) {
+                    Log.e(null, "An error occurred taking the picture", ex);
+                }
+
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            try {
+                Thread.sleep(200);
+            } catch (Exception ex) {
+            }
+            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
             ImageView imageView = (ImageView)findViewById(R.id.PreviewImageView);
             imageView.setImageBitmap(imageBitmap);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
